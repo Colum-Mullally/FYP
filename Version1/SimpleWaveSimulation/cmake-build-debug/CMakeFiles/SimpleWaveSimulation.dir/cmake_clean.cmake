dfdcb519ce94dfb8a3524066481a3d4d60589cc1
Colum Mullally <15142434@studentmail.ul.ie>
2018-09-25 20:26:50 +0100

    Init

---
+++ Version1/SimpleWaveSimulation/cmake-build-debug/CMakeFiles/SimpleWaveSimulation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleWaveSimulation.dir/main.cpp.obj"
+  "SimpleWaveSimulation.pdb"
+  "SimpleWaveSimulation.exe"
+  "SimpleWaveSimulation.exe.manifest"
+  "libSimpleWaveSimulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleWaveSimulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
