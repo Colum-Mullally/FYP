85e8338040d6dbf2899986b71adc47f17128c706
Colum Mullally <15142434@studentmail.ul.ie>
2018-09-26 15:39:28 +0100

    Basic calculations No visualization

--- Version1/SimpleWaveSimulation/main.cpp
+++ Version1/SimpleWaveSimulation/main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <cstdio>
+void updateWave (const double timeInterval, double* x, double* speed)
+{
+    //wave translation
+    (*x) += timeInterval * (*speed);
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //wall collision
+    if((*x) > 1.0)
+    {
+        (*speed) *= -1.0;
+        (*x) += 1.0 +timeInterval * (*speed);
+    } else if((*x) < 0.0)
+    {
+        (*speed) *= -1.0;
+        (*x) += timeInterval * (*speed);
+    }
+
+}
+
+int main()
+{
+    double x = 0.0;
+    double y = 1.0;
+    double speedX = 1.0;
+    double speedY = -0.5;
+
+    const int fps = 60;
+    const double timeInterval = 1.0/fps;
+
+    for (int i = 0; i<1000; ++i)
+    {
+        //wave update
+        updateWave(timeInterval, &x, &speedX);
+        updateWave(timeInterval, &y, &speedY);
+    }
     return 0;
 }
